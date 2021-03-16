cba814ab69e988d5d6e778a6f81239c3187e34e2
serinuntius <wasab1gj@gmail.com>
2021-03-16 13:22:25 +0900

    optimismのplugin入れてcompilerのバージョン変えた

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,6 @@
 import { HardhatUserConfig } from 'hardhat/config';
+import '@eth-optimism/plugins/hardhat/compiler'
+
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
 import 'hardhat-typechain';
@@ -9,18 +11,7 @@ import 'hardhat-deploy';
 // defaultNetwork, networks, solc, and paths.
 // Go to https://buidler.dev/config/ to learn more
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-      {
-        version: '0.6.8',
-        settings: {
-          optimizer: {
-            enabled: true,
-          },
-        },
-      },
-    ],
-  },
+  solidity: '0.6.12'
 };
 
 export default config;
